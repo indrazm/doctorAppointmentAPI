04145455607f0e0094c8a21bd458ed83b6b1090d
Indra Zulfi <me@indrazm.com>
2023-06-02 12:42:32 +0700

    API

--- src/app/api/v1/doctors/route.ts
+++ src/app/api/v1/doctors/route.ts
@@ -2,24 +2,14 @@ import { NextRequest, NextResponse } from "next/server"
 import { prisma } from "@/lib/prismaClient"
 
 export async function GET(req: NextRequest) {
-    "use server"
-    const doctorId = req.nextUrl.searchParams.get("doctorId")
-    if (doctorId) {
-        const doctor = await prisma.doctor.findFirst({
-            where: {
-                id: doctorId,
-            },
+    try {
+        const doctors = await prisma.doctor.findMany({
             include: {
                 poli: true,
             },
         })
-        return NextResponse.json({ status: 200, message: "Fetch single successful", data: doctor })
+        return NextResponse.json({ status: 200, message: "Fetch successful", data: doctors })
+    } catch (error) {
+        return NextResponse.json({ error })
     }
-
-    const doctors = await prisma.doctor.findMany({
-        include: {
-            poli: true,
-        },
-    })
-    return NextResponse.json({ status: 200, message: "Fetch successful", data: doctors })
 }
