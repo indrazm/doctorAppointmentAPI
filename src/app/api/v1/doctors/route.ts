58ab3d770279a97ac46db4096512b96bb1b0c3c9
Indra Zulfi <me@indrazm.com>
2023-06-02 12:48:50 +0700

    API

--- src/app/api/v1/doctors/route.ts
+++ src/app/api/v1/doctors/route.ts
@@ -2,14 +2,10 @@ import { NextRequest, NextResponse } from "next/server"
 import { prisma } from "@/lib/prismaClient"
 
 export async function GET(req: NextRequest) {
-    try {
-        const doctors = await prisma.doctor.findMany({
-            include: {
-                poli: true,
-            },
-        })
-        return NextResponse.json({ status: 200, message: "Fetch successful", data: doctors })
-    } catch (error) {
-        return NextResponse.json({ error })
-    }
+    const doctors = await prisma.doctor.findMany({
+        include: {
+            poli: true,
+        },
+    })
+    return NextResponse.json({ status: 200, message: "Fetch successful", data: doctors })
 }
